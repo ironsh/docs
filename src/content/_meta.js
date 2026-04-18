31286ce60479535e938c14f3df890a3a7361c000
Matthew Slipper <me@matthewslipper.com>
2026-04-18 00:32:48 -0600

    docs: add control plane section with overview, enrollment, and policies (#40)

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: "Overview",
   quickstart: "Quickstart",
   help: "Getting Help",
+  "control-plane": "Control Plane",
   guides: "Guides",
   reference: "Reference",
   legacy: "Legacy Platform",
